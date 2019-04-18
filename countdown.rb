130094fc12d44184825364c2bf8f962624a29a72
Jess Rezac <jess.rezac@gmail.com>
2019-04-18 03:31:22 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
 def countdown
+  seconds = 10
+  while seconds > 0 do
+    puts "#{seconds} SECOND(S)!"
+    seconds = seconds - 1
+  end
+
+  puts "HAPPY NEW YEAR!"  
 end
